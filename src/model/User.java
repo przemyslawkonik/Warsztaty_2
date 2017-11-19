02118fbfce427f8b2564e1e99f2c03bbae58455b
Przemysław Konik <przemyslawkonik@gmail.com>
2017-11-19 16:43:33 +0100

    add User class

--- src/model/User.java
+++ src/model/User.java
@@ -1,6 +1,11 @@
 package model;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.mindrot.jbcrypt.BCrypt;
 
 public class User {
 	private long id;
@@ -9,18 +14,55 @@ public class User {
 	private String password;
 	private int userGroupId;
 
+	public User() {
+		this.id = 0l;
+		this.username = "";
+		this.email = "";
+		this.password = "";
+		this.userGroupId = 0;
+	}
+
 	public User(String username, String email, String password) {
 		this.id = 0l;
 		this.username = username;
 		this.email = email;
-		this.password = password;
+		setPassword(password);
 		this.userGroupId = 0;
 	}
 
-	public void save(Connection conn) {
+	public static User getById(long id) {
+		String sql = "";
+		// execute sql
+		User u = new User();
+		return u;
+	}
+
+	public void save(Connection conn) throws SQLException {
 		if (this.id == 0) {
 			String sql = "INSERT INTO users(username, email, password, user_group_id) " + "VALUES(?, ?, ?, ?)";
-			
+			String[] generatedColumns = { "id" };
+			PreparedStatement ps = conn.prepareStatement(sql, generatedColumns);
+			ps.setString(1, this.username);
+			ps.setString(2, this.email);
+			ps.setString(3, this.password);
+			ps.setInt(4, this.userGroupId);
+			ps.executeUpdate();
+			ResultSet gk = ps.getGeneratedKeys();
+			if (gk.next()) {
+				this.id = gk.getLong(1);
+			}
+			gk.close();
+			ps.close();
+		} else {
+			String sql = "UPDATE users SET username=?, email=?, password=?, user_group_id=? WHERE id=?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, this.username);
+			ps.setString(2, this.email);
+			ps.setString(3, this.password);
+			ps.setInt(4, this.userGroupId);
+			ps.setLong(5, this.id);
+			ps.executeUpdate();
+			ps.close();
 		}
 	}
 
@@ -45,7 +87,11 @@ public class User {
 	}
 
 	public void setPassword(String password) {
-		this.password = password;
+		this.password = BCrypt.hashpw(password, BCrypt.gensalt());
+	}
+
+	public boolean checkPassword(String password) {
+		return BCrypt.checkpw(password, this.password);
 	}
 
 	public int getUserGroupId() {
