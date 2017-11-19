ec56029910c94c87faff1be0635ccb7cb739aa4a
Przemysław Konik <przemyslawkonik@gmail.com>
2017-11-19 19:17:53 +0100

    update db script

--- sql/db.sql
+++ sql/db.sql
@@ -5,11 +5,30 @@ CREATE TABLE user_group(
 );
 
 CREATE TABLE users(
-	id BIGINT(20) AUTO_INCREMENT,
+	id BIGINT AUTO_INCREMENT,
 	username VARCHAR(255),
 	email VARCHAR(255) UNIQUE,
 	password VARCHAR(245),
 	user_group_id INT,
 	PRIMARY KEY(id),
 	FOREIGN KEY(user_group_id) REFERENCES user_group(id)
+);
+
+CREATE TABLE excercise(
+	id INT AUTO_INCREMENT,
+	title VARCHAR(255),
+	description TEXT,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE solution(
+	id INT AUTO_INCREMENT,
+	created DATETIME,
+	updated DATETIME,
+	description TEXT,
+	excercise_id INT,
+	users_id BIGINT,
+	PRIMARY KEY(id),
+	FOREIGN KEY(excercise_id) REFERENCES excercise(id),
+	FOREIGN KEY(users_id) REFERENCES users(id)
 );
