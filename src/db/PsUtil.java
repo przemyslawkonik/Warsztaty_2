c9a939b7592578b65258268f7e1279a4b8df9a14
Przemysław Konik <przemyslawkonik@gmail.com>
2017-11-28 19:10:20 +0100

    changes to PsUtil

--- src/db/PsUtil.java
+++ src/db/PsUtil.java
@@ -10,92 +10,80 @@ import model.User;
 
 public class PsUtil {
 
-	public static void prepare(PreparedStatement ps, Operation o, User u) throws SQLException {
-		switch (o) {
-		case INSERT: {
-			ps.setString(1, u.getUsername());
-			ps.setString(2, u.getEmail());
-			ps.setString(3, u.getPassword());
-			ps.setInt(4, u.getUserGroupId());
-			break;
-		}
-		case UPDATE: {
-			ps.setString(1, u.getUsername());
-			ps.setString(2, u.getEmail());
-			ps.setString(3, u.getPassword());
-			ps.setInt(4, u.getUserGroupId());
-			ps.setLong(5, u.getId());
-			break;
-		}
-		case DELETE: {
-			ps.setLong(1, u.getId());
-			break;
-		}
-		}
-	}
+    public static void prepare(PreparedStatement ps, Operation o, User u) throws SQLException {
+        switch (o) {
+            case UPDATE: {
+                ps.setLong(5, u.getId());
+                //no break
+            }
+            case INSERT: {
+                ps.setString(1, u.getUsername());
+                ps.setString(2, u.getEmail());
+                ps.setString(3, u.getPassword());
+                ps.setInt(4, u.getUserGroupId());
+                break;
+            }
+            case DELETE: {
+                ps.setLong(1, u.getId());
+                break;
+            }
+        }
+    }
 
-	public static void prepare(PreparedStatement ps, Operation o, Group g) throws SQLException {
-		switch (o) {
-		case INSERT: {
-			ps.setString(1, g.getName());
-			break;
-		}
-		case UPDATE: {
-			ps.setString(1, g.getName());
-			ps.setInt(2, g.getId());
-			break;
-		}
-		case DELETE: {
-			ps.setInt(1, g.getId());
-			break;
-		}
-		}
-	}
+    public static void prepare(PreparedStatement ps, Operation o, Group g) throws SQLException {
+        switch (o) {
+            case UPDATE: {
+                ps.setInt(2, g.getId());
+                //no break
+            }
+            case INSERT: {
+                ps.setString(1, g.getName());
+                break;
+            }
+            case DELETE: {
+                ps.setInt(1, g.getId());
+                break;
+            }
+        }
+    }
 
-	public static void prepare(PreparedStatement ps, Operation o, Excercise e) throws SQLException {
-		switch (o) {
-		case INSERT: {
-			ps.setString(1, e.getTitle());
-			ps.setString(2, e.getDescription());
-			break;
-		}
-		case UPDATE: {
-			ps.setString(1, e.getTitle());
-			ps.setString(2, e.getDescription());
-			ps.setInt(3, e.getId());
-			break;
-		}
-		case DELETE: {
-			ps.setInt(1, e.getId());
-			break;
-		}
-		}
-	}
+    public static void prepare(PreparedStatement ps, Operation o, Excercise e) throws SQLException {
+        switch (o) {
+            case UPDATE: {
+                ps.setInt(3, e.getId());
+                //no break
+            }
+            case INSERT: {
+                ps.setString(1, e.getTitle());
+                ps.setString(2, e.getDescription());
+                break;
+            }
+            case DELETE: {
+                ps.setInt(1, e.getId());
+                break;
+            }
+        }
+    }
 
-	public static void prepare(PreparedStatement ps, Operation o, Solution s) throws SQLException {
-		switch (o) {
-		case INSERT: {
-			ps.setString(1, s.getCreated());
-			ps.setString(2, s.getUpdated());
-			ps.setString(3, s.getDescription());
-			ps.setInt(4, s.getExcerciseId());
-			ps.setLong(5, s.getUserId());
-			break;
-		}
-		case UPDATE: {
-			ps.setString(1, s.getCreated());
-			ps.setString(2, s.getUpdated());
-			ps.setString(3, s.getDescription());
-			ps.setInt(4, s.getExcerciseId());
-			ps.setLong(5, s.getUserId());
-			ps.setInt(6, s.getId());
-			break;
-		}
-		case DELETE: {
-			ps.setInt(1, s.getId());
-			break;
-		}
-		}
-	}
+    public static void prepare(PreparedStatement ps, Operation o, Solution s) throws SQLException {
+        switch (o) {
+            case UPDATE: {
+                ps.setInt(6, s.getId());
+                //no break
+            }
+            case INSERT: {
+                ps.setString(1, s.getCreated());
+                ps.setString(2, s.getUpdated());
+                ps.setString(3, s.getDescription());
+                ps.setInt(4, s.getExcerciseId());
+                ps.setLong(5, s.getUserId());
+                break;
+            }
+            case DELETE: {
+                ps.setInt(1, s.getId());
+                break;
+            }
+        }
+    }
 
 }
