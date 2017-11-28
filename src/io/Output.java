a398cbd262fa95b234e11b78d7f525085a50a16e
Przemysław Konik <przemyslawkonik@gmail.com>
2017-11-28 18:35:10 +0100

    extract separator to variable

--- src/io/Output.java
+++ src/io/Output.java
@@ -8,30 +8,39 @@ import model.Solution;
 import model.User;
 
 public class Output {
+	public static String separator = " | ";
 
 	public static void printUsers(List<User> users) {
 		for (User u : users) {
-			System.out.println(u.getId() + " | " + u.getUsername() + " | " + u.getEmail() + " | " + u.getUserGroupId());
+			System.out.println(u.getId() + separator + u.getUsername() + separator + u.getEmail() + separator + u.getUserGroupId());
 		}
 	}
 
 	public static void printGroups(List<Group> groups) {
 		for (Group g : groups) {
-			System.out.println(g.getId() + " | " + g.getName());
+			System.out.println(g.getId() + separator + g.getName());
 		}
 	}
 
 	public static void printExcercises(List<Excercise> excercises) {
 		for (Excercise e : excercises) {
-			System.out.println(e.getId() + " | " + e.getTitle() + " | " + e.getDescription());
+			System.out.println(e.getId() + separator + e.getTitle() + separator + e.getDescription());
 		}
 	}
 
 	public static void printSolutions(List<Solution> solutions) {
 		for (Solution s : solutions) {
-			System.out.println(s.getId() + " | " + s.getCreated() + " | " + s.getUpdated() + " | " + s.getDescription()
-					+ " | " + s.getExcerciseId() + " | " + s.getUserId());
+			System.out.println(s.getId() + separator + s.getCreated() + separator + s.getUpdated() + separator + s.getDescription()
+					+ separator + s.getExcerciseId() + separator + s.getUserId());
 		}
+	}
+
+	public static String getSeparator() {
+		return separator;
+	}
+
+	public static void setSeparator(String separator) {
+		Output.separator = separator;
 	}
 
 }
